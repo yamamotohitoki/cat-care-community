96c2c6bcfc1df479f240a4d5b9361388c03234bd
EC2 Default User <ec2-user@ip-172-31-31-92.ap-northeast-1.compute.internal>
2023-09-26 07:13:28 +0000

    [add] seedファイル作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,133 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.create!(
-  email: "admin@email.com",
-  password: "adminpath"
-)
+
+Admin.find_or_create_by!(email: "admin@email.com") do |admin|
+  admin.password = "adminpath"
+end
+
+olivia = Member.find_or_create_by!(email: "olivia@example.com") do |member|
+  member.name = "オリビア"
+  member.password = "password"
+  member.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-member3.jpg"), filename:"sample-member1.jpg")
+end
+
+james = Member.find_or_create_by!(email: "james@example.com") do |member|
+  member.name = "ジェームズ"
+  member.password = "password"
+  member.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-member1.jpg"), filename:"sample-member2.jpg")
+end
+
+lucas = Member.find_or_create_by!(email: "lucas@example.com") do |member|
+  member.name = "ルークス"
+  member.password = "password"
+  member.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-member2.jpg"), filename:"sample-member3.jpg")
+end
+
+Blog.find_or_create_by!(title: "今日の食事") do |blog|
+  blog.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-blog1.jpg"), filename:"sample-blog1.jpg")
+  blog.body = "仲良く食事していました！"
+  blog.status = 0
+  blog.member = olivia
+end
+
+Blog.find_or_create_by!(title: "お散歩に行ってきました！") do |blog|
+  blog.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-blog2.jpg"), filename:"sample-blog2.jpg")
+  blog.body = "天気が良くて猫ちゃんも幸せそうでした！"
+  blog.status = 0
+  blog.member = james
+end
+
+Blog.find_or_create_by!(title: "大嫌いな病院") do |blog|
+  blog.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-blog3.jpg"), filename:"sample-blog3.jpg")
+  blog.body = "大嫌いな病院に頑張って行っていきました！お利口さん！"
+  blog.status = 0
+  blog.member = lucas
+end
+
+breed_names = [
+  "ミックス",
+  "スコティッシュフォールド",
+  "マンチカン",
+  "アメリカンショートヘア",
+  "ラグドール",
+  "ブリティッシュショートヘア",
+  "ノルウェージャンフォレストキャット",
+  "サイベリアン",
+  "ロシアンブルー",
+  "ベンガル"
+]
+
+breed_names.each do |name|
+  Breed.create!(name: name)
+end
+
+
+Cat.find_or_create_by!(name: "太郎") do |cat|
+  cat.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-cat1.jpg"), filename:"sample-cat1.jpg")
+  cat.breed = Breed.find_by(name: "スコティッシュフォールド")
+  cat.introduction = "人見知りの男の子です！"
+  cat.birthday = Date.new(2021, 12, 1)
+  cat.family_join_day = Date.new(2021, 2, 5)
+  cat.weight = 6
+  cat.ideal_weight = 5
+  cat.sex = 0
+  cat.member = olivia
+end
+
+Cat.find_or_create_by!(name: "マリン") do |cat|
+  cat.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-cat2.jpg"), filename:"sample-cat2.jpg")
+  cat.breed = Breed.find_by(name: "マンチカン")
+  cat.introduction = "おてんばな女の子です！"
+  cat.birthday = Date.new(2022, 8, 4)
+  cat.family_join_day = Date.new(2022, 9, 15)
+  cat.weight = 7.4
+  cat.ideal_weight = 6.3
+  cat.sex = 1
+  cat.member = james
+end
+
+Cat.find_or_create_by!(name: "タマ") do |cat|
+  cat.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-cat3.jpg"), filename:"sample-cat3.jpg")
+  cat.breed = Breed.find_by(name: "アメリカンショートヘア")
+  cat.introduction = "寝ることが大好きです！"
+  cat.sex = 0
+  cat.birthday = Date.new(2023, 3, 19)
+  cat.family_join_day = Date.new(2023, 7, 21)
+  cat.weight = 4
+  cat.ideal_weight = 3.2
+  cat.member = lucas
+end
+
+Community.find_or_create_by!(title: "ブリちゃん同好会") do |community|
+  community.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-community1.jpg"), filename:"sample-community1.jpg")
+  community.body = "このコミュニティは、愛らしいさと知性あるこの猫種を愛する人々のための場所です。猫の飼育、健康、トリビアについての情報を共有し、仲間とのつながりを楽しみましょう。"
+end
+
+Community.find_or_create_by!(title: "スコティッシュ研究所") do |community|
+  community.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-community2.jpg"), filename:"sample-community2.jpg")
+  community.body = "このコミュニティは、スコティッシュフォールドを愛し、共有する場所です。健康ケアや楽しいエピソードの共有、ファン同士の交流をお楽しみください。"
+end
+
+Community.find_or_create_by!(title: "マンチカンの里") do |community|
+  community.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-community3.jpg"), filename:"sample-community3.jpg")
+  community.body = "このコミュニティは、短足で愛らしいマンチカン猫のファンが集まる場所です。飼育アドバイスやカウンセリングの瞬間の共有、仲間との交流をお楽しみください。"
+end
+
+Topic.find_or_create_by!(title: "このおもちゃがおすすめです！") do |topic|
+  topic.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-topic1.jpg"), filename:"sample-topic1.jpg")
+  topic.body = "猫じゃらしのおもちゃでずーっと遊んでいます！"
+  topic.community = Community.find_by(title: "ブリちゃん同好会")
+end
+
+Topic.find_or_create_by!(title: "このフードがおすすめです！") do |topic|
+  topic.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-topic2.jpg"), filename:"sample-topic2.jpg")
+  topic.body = "このフードに変えてから減量に成功しました！"
+  topic.community = Community.find_by(title: "スコティッシュ研究所")
+end
+
+Topic.find_or_create_by!(title: "やっぱりチュールはまぐろ味!") do |topic|
+  topic.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-topic2.jpg"), filename:"sample-topic2.jpg")
+  topic.body = "みなさんのおすすめのおやつはありますか？"
+  topic.community = Community.find_by(title: "マンチカンの里")
+end
